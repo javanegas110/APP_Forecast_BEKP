25a519f90e3a5044e730fe654e17f9db78971729
javanegas110 <daemon110@gmail.com>
2026-04-04 23:47:04 -0500

    Update App_BEKP.py

--- App_BEKP.py
+++ App_BEKP.py
@@ -22,16 +22,15 @@ import streamlit as st
 
 #Personalizacion app
 st.set_page_config(
-    page_title="Forecast BEKP",
-    page_icon="assets/logo_essity.png",
-    layout="wide"
+        page_icon="assets/logo_essity.png",
+    layout="centered"
 )
 
 # Header con logo
 col1, col2 = st.columns([1,6])
 
 with col1:
-    st.image("assets/logo_essity.png", width=150)
+    st.image("assets/logo_essity.png", width=180)
 
 with col2:
     st.markdown(
